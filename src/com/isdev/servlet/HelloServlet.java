3aae93f3778af23fec56de64300a4619d9b8fdca
pangge <869213686@qq.com>
2018-07-06 01:52:08 +0800

    request encoding

--- src/com/isdev/servlet/HelloServlet.java
+++ src/com/isdev/servlet/HelloServlet.java
@@ -2,6 +2,7 @@ package com.isdev.servlet;
 
 import javax.servlet.ServletException;
 import javax.servlet.ServletInputStream;
+import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -16,19 +17,59 @@ public class HelloServlet extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-        doPost(req, resp);
+//        doPost(req, resp);
+//        String params = req.getQueryString();
+//        System.out.println("get params = " + params);
+//
+
     }
 
     @Override
     protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 //        requestLine(req);
 //        requestHeaders(req, resp);
+//        requestData(req);
+        /**
+         * 设值参数的查询码表
+         * 该方法支队请求实体内容起作用（即POST请求的参数）
+         * get请求可以通过修改：tomcat->conf->server.xml
+         * <Connector port="8080" protocol="HTTP/1.1"
+         *     connectionTimeout="20000"
+         *     redirectPort="8443"
+         *     URIEncoding="utf-8"
+         />
+         * 或者：String name = new String(name.getBytes("iso-8859-1"),"utf-8");
+         */
+        req.setCharacterEncoding("UTF-8");
+//        resp.setContentType("text/html;charset=UTF-8");
+        Enumeration<String> parameterNames = req.getParameterNames();
+        while(parameterNames.hasMoreElements()) {
+            String name = parameterNames.nextElement();
+            if(name.equals("hobby")) {
+                System.out.print("爱好：");
+                String[] hobbys = req.getParameterValues(name);
+                for (String hobby:hobbys) {
+                    System.out.print(hobby + ":");
+                }
+                System.out.println();
+            } else {
+                System.out.println(name + ":" + req.getParameter(name));
+            }
+        }
+        ServletOutputStream outputStream = resp.getOutputStream();
+        outputStream.println("qwert");
+//        resp.getWriter().println("请求成功");
+
+    }
+
+    //请求参数，不知道为啥不能用
+    private void requestData(HttpServletRequest req) throws IOException {
         ServletInputStream inputStream = req.getInputStream();
         int len = 0;
         byte[] buf = new byte[1024];
         while((len = inputStream.read())!=-1) {
             String str = new String(buf, 0, len);
-            System.out.println(str);
+            System.out.print(str);
         }
     }
 
--- web/index.jsp
+++ web/index.jsp
@@ -11,6 +11,22 @@
     <title>$Title$</title>
   </head>
   <body>
-  $END$
+  GET提交
+  <form action="/hello" method="get">
+    请输入姓名：<input name="user" type="text" value="">
+    <input type="submit" value="确定" />
+  </form>
+  POST提交
+  <form action="/hello" method="post">
+    请输入姓名：<input name="user" type="text" value=""><br/>
+    请输入密码：<input name="pass" type="password" value=""><br/>
+    性别：<input type="radio" name="sex" value="男">男
+          <input type="radio" name="sex" value="女">女<br/>
+    爱好：<input type="checkbox" name="hobby" value="篮球">篮球
+          <input type="checkbox" name="hobby" value="篮球">足球
+          <input type="checkbox" name="hobby" value="篮球">羽毛球<br/>
+    描述：<textarea cols="40" rows="10" name="desc"></textarea><br/>
+    <input type="submit" value="确定" />
+  </form>
   </body>
 </html>
